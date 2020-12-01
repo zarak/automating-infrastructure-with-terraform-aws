08c11caad088f4a944a36ff7dcbfae247e23aac5
Zarak <7254237+zarak@users.noreply.github.com>
2020-12-01 12:29:47 +0500

    Set autoscaling outputs

--- module_2/modules/autoscaling/main.tf
+++ module_2/modules/autoscaling/main.tf
@@ -26,9 +26,35 @@ resource "aws_launch_template" "webserver" {
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   user_data     = data.template_cloudinit_config.config.rendered
-  key_name      = var.ssh_keypair
+  key_name            = var.ssh_keypair
   iam_instance_profile {
     name = module.iam_instance_profile.name
   }
   vpc_security_group_ids = [var.sg.websvr]
+}
+ 
+resource "aws_autoscaling_group" "webserver" {
+  name                = "${var.namespace}-asg"
+  min_size            = 1
+  max_size            = 3
+  vpc_zone_identifier = var.vpc.private_subnets
+  target_group_arns   = module.alb.target_group_arns
+  launch_template {
+    id = aws_launch_template.webserver.id
+    version = aws_launch_template.webserver.latest_version
+  }
+}
+ 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 4.0"
+  load_balancer_name       = "${var.namespace}-alb"
+  security_groups          = [var.sg.lb]
+  subnets                  = var.vpc.public_subnets
+  vpc_id                   = var.vpc.vpc_id
+  logging_enabled          = false
+  http_tcp_listeners       = [{ port = 80, protocol = "HTTP" }]
+  http_tcp_listeners_count = "1"
+  target_groups            = [{ name = "websvr", backend_protocol = "HTTP", backend_port = 8080 }]
+  target_groups_count      = "1"
 }
