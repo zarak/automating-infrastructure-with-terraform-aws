aa08b2d06db59fb1480c702edf84e006ba1bb10f
Zarak <7254237+zarak@users.noreply.github.com>
2020-12-01 11:41:39 +0500

    Trickle data down to db module

--- module_2/main.tf
+++ module_2/main.tf
@@ -1,3 +1,11 @@
+module "database" {
+  source = "./modules/database"
+  namespace = var.namespace
+
+  vpc = module.networking.vpc
+  sg = module.networking.sg
+}
+
 module "networking" {
   source = "./modules/networking"
   namespace = var.namespace
